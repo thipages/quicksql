ab1c8be9144c644344fa2745573a08b6dbea3197
thipages <thipages@gmail.com>
2020-04-05 17:05:57 +0200

    add single file quicksql.php

---
+++ quicksql.php
@@ -0,0 +1,21 @@
+<?php
+class QSql {
+    public static function insert ($tableName, $keyValues) {
+        return join(" ",[
+            "INSERT INTO $tableName (",
+            join(',',array_keys($keyValues)),
+            ") VALUES (",
+            join(',',array_map(function($v){return "'$v'";},array_values($keyValues))),
+            ");"
+        ]);
+    }
+    public static function update($tableName, $keyValues,$where) {
+        $set=[];
+        foreach ($keyValues as $key=>$value) $set[]="$key='$value'";
+        return join(" ",[
+            "UPDATE $tableName SET",
+            join(",",$set),
+            "WHERE ".$where.";"
+        ]);
+    }
+}
